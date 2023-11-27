c6431b1870819a3a735fb5eade2b56166653617b
hamzakamran <hamzakwebdev@gmail.com>
2023-11-27 14:58:48 -0600

    CORS

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ dotenv.config();
 connectDb();
 
 // app.use(credentials);
-app.use(cors());
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			callback(null, true);
+		},
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
