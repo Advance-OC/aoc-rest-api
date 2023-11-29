f0e28dc1096cb785e651f567b9f7f5ddacbd21f4
hamzakamran <hamzakwebdev@gmail.com>
2023-11-29 01:34:35 -0600

    Test

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const connectDb = require("./config/db");
 const dotenv = require("dotenv");
+dotenv.config();
 const verifyToken = require("./middleware/verifyToken");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-dotenv.config();
 connectDb();
 
 app.use(
