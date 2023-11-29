a78d7a9d0b221cbf39c371cb50dcfe452f9255b9
hamzakamran <hamzakwebdev@gmail.com>
2023-11-29 01:42:25 -0600

    Updated auth

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -4,7 +4,6 @@ const verifyToken = (req, res, next) => {
 	const authHeader = req.headers["authorization"];
 	if (!authHeader) return res.sendStatus(401);
 	const token = authHeader.split(" ")[1];
-	console.log(token, process.env.API_TOKEN);
 	if (token === process.env.API_TOKEN) {
 		next();
 	} else {
